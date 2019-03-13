367c84d4fd1ef4cd85a64d469f92062035f3464e
Aurimas <aurimas@darguzis.com>
2019-03-13 15:22:38 +0000

    created all action creators

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,7 +25,10 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {}
+  onSubmit = formValues => {
+    console.log('$$: ', this.props);
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -56,7 +61,12 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
